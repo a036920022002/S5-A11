0e49cd857d1cbf2341d94aedb72b83d3d4acd2c3
Yuchen <chengcheng0805@gmail.com>
2022-09-16 17:02:07 +0800

    add mongoose &dotenv setting

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
-
-
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const app = express()
 const port = 3000
-
+const MY_ENV = process.env.MY_ENV
+mongoose.connect(MY_ENV, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('connect error')
+})
+db.once('open', () => {
+  console.log('mongodb connected!!')
+})
 
 app.get('/', (req, res) => {
   res.send('new express')
