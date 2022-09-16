6d04093f681682ec4e5f412ef3b74eb958d43b31
Yuchen <chengcheng0805@gmail.com>
2022-09-16 17:32:45 +0800

    the seeder is not yet

---
+++ models/seeds/URLseeder.js
@@ -0,0 +1,12 @@
+require('../../config/mongoose')
+
+const URL = require('../originalURL')
+const db = require('../../config/mongoose')
+
+db.once
+
+function createshort() {
+  const numbers = '1234567890'
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseLetters = lowerCaseLetters.toUpperCase()
+}
