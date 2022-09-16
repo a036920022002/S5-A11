87f283c75f9038f4daf0284591dbead6363a135e
Yuchen <chengcheng0805@gmail.com>
2022-09-16 17:25:12 +0800

    add mongoose Schema

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,9 +4,12 @@ require('dotenv').config()
 const MY_ENV = process.env.MY_ENV
 mongoose.connect(MY_ENV, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
+
 db.on('error', () => {
   console.log('connect error')
 })
 db.once('open', () => {
   console.log('mongodb connected!!')
 })
+
+module.exports = db
---
+++ models/originalURL.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const URLSchema = new Schema({
+  originURL: { type: String, required: true },
+  shortURL: { type: String, required: true }
+})
+
+module.exports = mongoose.model('URL', URLSchema)
