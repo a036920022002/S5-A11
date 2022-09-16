4a93f4696c2397d084d80121218e677f88a79b0c
Yuchen <chengcheng0805@gmail.com>
2022-09-16 17:04:14 +0800

    add mongoose seeting to config

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const express = require('express')
-const mongoose = require('mongoose')
-require('dotenv').config()
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
-const MY_ENV = process.env.MY_ENV
-mongoose.connect(MY_ENV, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('connect error')
-})
-db.once('open', () => {
-  console.log('mongodb connected!!')
-})
+
 
 app.get('/', (req, res) => {
-  res.send('new express')
+  res.send('new express+mongoose')
 })
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
---
+++ config/mongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const MY_ENV = process.env.MY_ENV
+mongoose.connect(MY_ENV, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('connect error')
+})
+db.once('open', () => {
+  console.log('mongodb connected!!')
+})
